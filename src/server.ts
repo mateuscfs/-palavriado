2a38d2b69629f68b423e121a1a89bfec51e896ae
Mateus Coelho <mateuscfs@gmail.com>
2022-02-15 00:51:10 -0300

    feat: add base files

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import app from './app';
+
+import connect from './database/connection';
+
+const PORT = process.env.PORT || 3000;
+
+async function startServer() {
+    connect();
+
+    app.listen(PORT, () => {
+        // eslint-disable-next-line no-console
+        console.log(`Palavriado Aulaine`);
+    });
+}
+
+startServer();
