f4b0023da3cb79f283a7247eac16ed105fd8b413
Mateus Coelho <mateuscfs@gmail.com>
2022-02-15 00:51:39 -0300

    feat: add index route

---
+++ src/routes/index.ts
@@ -0,0 +1,15 @@
+import { Request, Response, Router } from 'express';
+
+import { swaggerSpec } from '../swagger';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+    res.send('Palavriado 1.0.0');
+});
+
+router.get('/api-docs', (req: Request, res: Response) => {
+    res.send(swaggerSpec);
+});
+
+export default router;
