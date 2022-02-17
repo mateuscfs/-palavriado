729daf8a73cc5bb28f72935e27470b18d76293c6
Mateus Coelho <mateuscfs@gmail.com>
2022-02-17 10:28:32 -0300

    feat: add getAll dictionary route

--- src/routes/dictionary.ts
+++ src/routes/dictionary.ts
@@ -1,5 +1,5 @@
-import { Router, Request, Response } from "express";
-import { createDictionary } from "../controllers/dictionary";
+import { Router, Request, Response } from 'express';
+import { createDictionary, getAllDictionary } from '../controllers/dictionary';
 
 const routes = Router();
 
@@ -9,6 +9,12 @@ routes.post('/', async (req: Request, res: Response) => {
     const response = await createDictionary(body);
 
     return res.status(201).json(response);
-})
+});
+
+routes.get('/', async (req: Request, res: Response) => {
+    const response = await getAllDictionary();
+
+    return res.status(200).json(response);
+});
 
 export const dictionaryRoutes: Router = routes;
