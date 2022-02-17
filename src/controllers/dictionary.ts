8857c35d932bf9d5517f4f6433a60e573f10a796
Mateus Coelho <mateuscfs@gmail.com>
2022-02-17 10:28:16 -0300

    feat: add onConflict on create + getAll Dictionary

--- src/controllers/dictionary.ts
+++ src/controllers/dictionary.ts
@@ -1,8 +1,22 @@
-import { create } from "../repository/dictionary";
-import { buildWordsObject } from "../utils/dictionary/builder";
+import { DictionaryInterface } from '../interfaces/dictionary';
+import { create, getAll } from '../repository/dictionary';
+import { buildWordsObject } from '../utils/dictionary/builder';
 
-export const createDictionary = async (words: string[]) => {
+export const createDictionary = async (
+    words: string[],
+): Promise<{ data: Record<string, unknown>[] | string }> => {
     const data = buildWordsObject(words);
 
-    return create(data);
-}
+    const res = await create(data);
+
+    if (res.raw.length === 0)
+        return { data: 'Palavra(s) já existe(m) no dicionário!' };
+
+    return { data: res.raw };
+};
+
+export const getAllDictionary = async (): Promise<{
+    data: DictionaryInterface[];
+}> => {
+    return { data: await getAll() };
+};
--- src/repository/dictionary.ts
+++ src/repository/dictionary.ts
@@ -1,11 +1,22 @@
-import { getRepository } from "typeorm"
-import { DictionaryEntity } from "../database/entities/Dictionary"
-import { Dictionary } from "../interfaces/dictionary";
+import { getRepository, InsertResult } from 'typeorm';
+import { DictionaryEntity } from '../database/entities/Dictionary';
+import { DictionaryInterface } from '../interfaces/dictionary';
 
-
-export const create = (data: Dictionary[]) => {
+export const create = (data: DictionaryInterface[]): Promise<InsertResult> => {
     const repository = getRepository(DictionaryEntity);
     const entity = repository.create(data);
 
-    return repository.save(entity);
-}
+    return repository
+        .createQueryBuilder()
+        .insert()
+        .into(DictionaryEntity)
+        .values(entity)
+        .onConflict(`(name) DO NOTHING`)
+        .returning('*')
+        .execute();
+};
+
+export const getAll = (): Promise<DictionaryInterface[]> => {
+    const repository = getRepository(DictionaryEntity);
+    return repository.find();
+};
